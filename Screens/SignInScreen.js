f521e197ab317b6720e945a3a12880fbc9325974
jerinwilson7 <jerinwilson7@gmail.com>
2023-09-13 22:32:36 +0530

    basic design done

--- Screens/SignInScreen.js
+++ Screens/SignInScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 
 const SignInScreen = () => {
@@ -12,25 +12,36 @@ const SignInScreen = () => {
             Create Your Account
           </Text>
         </View>
-        {/* form */}
-        <View className=" flex mt-16 ">
-          <View className=" p-4 focus-within:shadow-lg space-x-2">
-            <Text className=" text-gray-500 font-gilroySemiBold text-base">
-              e-mail
-            </Text>
-            <TextInput
-              underlineColorAndroid="grey"
-              keyboardType="email-address"
-              className="mb-3 pb-1 pt-2"
-            />
-            <Text className=" text-gray-500 font-gilroySemiBold text-base">
-              Password
-            </Text>
-            <TextInput
-              underlineColorAndroid="grey"
-              secureTextEntry
-              className="mb-3 pb-1 pt-2"
-            />
+      </View>
+      {/* form */}
+      <View className=" flex mt-16 mx-4 space-y-4 ">
+        <View className=" p-4  focus-within:shadow-lg space-x-2">
+          <Text className=" text-gray-500 font-gilroySemiBold text-base">
+            Name
+          </Text>
+          <TextInput underlineColorAndroid="grey" className="mb-3 pb-1 pt-2" />
+          <Text className=" text-gray-500 font-gilroySemiBold text-base">
+            e-mail
+          </Text>
+          <TextInput
+            underlineColorAndroid="grey"
+            keyboardType="email-address"
+            className="mb-3 pb-1 pt-2"
+          />
+          <Text className=" text-gray-500 font-gilroySemiBold text-base">
+            Password
+          </Text>
+          <TextInput
+            underlineColorAndroid="grey"
+            secureTextEntry
+            className="mb-3 pb-1 pt-2"
+          />
+          <View className="w-full">
+            <TouchableOpacity className="w-full rounded-2xl bg-seaGreen p-5 mt-7">
+              <Text className="text-xl font-gilroyBold text-center text-white">
+                Signup
+              </Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
