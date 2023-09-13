99e6fc025641d32360520cca14161fc6dd550169
jerinwilson7 <jerinwilson7@gmail.com>
2023-09-13 23:04:51 +0530

    basic design completed

--- Screens/LogInScreen.js
+++ Screens/LogInScreen.js
@@ -1,12 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React from "react";
 
 const LogInScreen = () => {
   return (
-    <View>
-      <Text>LogInScreen</Text>
+    <View className="flex">
+      {/* Header */}
+      <View className="flex-shrink-0 bg-seaGreen w-full h-60 rounded-b-log">
+        <View className="pt-32 pl-8">
+          <Text className="text-white font-gilroyBold text-3xl">
+            Hey There !
+          </Text>
+          <Text className="text-white font-gilroyMedium text-lg">
+            Welcome back,{"\n"} continue your journey
+          </Text>
+        </View>
+      </View>
+      {/* form */}
+      <View className=" flex mt-16 mx-4 space-y-4 ">
+        <View className=" p-4  focus-within:shadow-lg space-x-2">
+          <Text className=" text-gray-500 font-gilroySemiBold text-base">
+            e-mail
+          </Text>
+          <TextInput
+            underlineColorAndroid="grey"
+            keyboardType="email-address"
+            className="mb-3 pb-1 pt-2"
+          />
+          <Text className=" text-gray-500 font-gilroySemiBold text-base">
+            Password
+          </Text>
+          <TextInput
+            underlineColorAndroid="grey"
+            secureTextEntry
+            className="mb-3 pb-1 pt-2"
+          />
+          <View className="w-full">
+            <TouchableOpacity className="w-full rounded-2xl bg-seaGreen p-5 mt-7">
+              <Text className="text-xl font-gilroyBold text-center text-white">
+                Login
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View className="flex-row justify-center">
+            <Text>Don't have an account?</Text>
+            <TouchableOpacity>
+              <Text className="text-sky-600">Signup </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default LogInScreen
+export default LogInScreen;
