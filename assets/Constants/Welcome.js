45ed22e86990b0a0a418ef2f098db2a2e4e4d650
jerinwilson7 <jerinwilson7@gmail.com>
2023-09-19 23:02:49 +0530

    updated onboarding contents

--- assets/Constants/Welcome.js
+++ assets/Constants/Welcome.js
@@ -4,8 +4,7 @@ const Onboarding_Contents = [
     image:
       "https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Discover Homemade Delicacies",
-    content:
-      "Explore a variety of homemade dishes crafted with love by talented home cooks in your neighborhood.",
+    content: "Embark on a culinary adventure right here on campus.",
   },
   {
     id: 2,
@@ -13,8 +12,7 @@ const Onboarding_Contents = [
       "https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 
     title: "Easy Ordering Process",
-    content:
-      "Ordering delicious, healthy, and hygienic meals has never been easier. Just a few taps and your meal is on its way.",
+    content: "Discover hidden gems and popular bites from our menu.",
   },
   {
     id: 3,
@@ -22,8 +20,7 @@ const Onboarding_Contents = [
       "https://images.pexels.com/photos/604969/pexels-photo-604969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 
     title: "Local, Fresh, and Convenient",
-    content:
-      "Enjoy locally sourced ingredients and support your community while savoring the convenience of doorstep delivery.",
+    content: "Get ready to savor each bite and explore the extraordinary.",
   },
 ];
 export default { Onboarding_Contents };
