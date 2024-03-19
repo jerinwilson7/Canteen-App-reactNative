430d3a5fc448c925a3ff13e7c78d45ba911f14d3
basil4693 <basilshaji4693@gmail.com>
2024-03-19 18:55:45 +0530

    updated store

--- Store/Store.js
+++ Store/Store.js
@@ -1,13 +1,4 @@
-// import { legacy_createStore as createStore, applyMiddleware } from "redux";
-// import Reducers from "../reducer";
-// import thunk from "redux-thunk";
 
-// const Store = createStore(Reducers, applyMiddleware(thunk));
-
-// const getToken = Store.getState().generalState.token;
-// console.log(getToken);
-
-// export { Store, getToken };
 
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import Reducers from "../reducer";
